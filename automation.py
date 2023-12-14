068a3b8ce5f201bdcef80ece6e2ab44585f2bba1
Robert Chapman <chapmanrobert12@gmail.com>
2023-12-14 10:46:05 -0600

    Created the python script file with required libraries imported

---
+++ automation.py
@@ -0,0 +1,8 @@
+# Libraries needed for the script
+import os
+import time
+import shutil
+import logging
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
