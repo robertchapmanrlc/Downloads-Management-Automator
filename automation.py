76e77d38ee3626398f4ba7d080a0b77c1d0e5df3
Robert Chapman <chapmanrobert12@gmail.com>
2023-12-14 11:22:22 -0600

    Created lists for file extensions for images, videos, audio, and other misc files

--- automation.py
+++ automation.py
@@ -6,3 +6,14 @@ import logging
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
+# Extensions for images
+images_extensions = [".png", ".jpg", ".jpeg", ".gif", ".webp"]
+
+# Extensions for audio files
+audio_extensions = [".mp3", ".wav"]
+
+# Extension for video files
+videos_extensions = [".mp4", ".mov"]
+
+# Extensions for various other types of files
+misc_extensions = [".pdf", ".pptx", ".docx", ".zip", ".txt", ".doc", ".csv"]
