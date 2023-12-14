4d40d72c6425a569a23c58758cfa4c0275e7b15c
Robert Chapman <chapmanrobert12@gmail.com>
2023-12-14 11:50:06 -0600

    Created 4 methods in the MoveHandler class to handle the types of files moved placed into the source folder

--- automation.py
+++ automation.py
@@ -29,10 +29,35 @@ misc_extensions = [".pdf", ".pptx", ".docx", ".zip", ".txt", ".doc", ".csv"]
 
 # Class that handles FileSystem events
 class MoveHandler(FileSystemEventHandler):
+    # This method will be ran whenever a file is moved into the "source_folder"
     def on_modified(self, event):
         with os.scandir(source_folder) as files:
             for file in files:
-                print(file.name)
+                self.handle_image_files(file.name)
+    
+    # This method detects and move images files
+    def handle_image_files(self, name):
+        for extension in images_extensions:
+            if name.endswith(extension):
+                print(f"moving image file: {name}")
+
+    # This method detects and move audio files
+    def handle_audio_files(self, name):
+        for extension in audio_extensions:
+            if name.endswith(extension):
+                print(f"moving audio file: {name}")
+
+    # This method detects and move video files
+    def handle_video_files(self, name):
+        for extension in videos_extensions:
+            if name.endswith(extension):
+                print(f"moving video file: {name}")
+
+    # This method detects and move various miscellaneous files
+    def handle_misc_files(self, name):
+        for extension in misc_extensions:
+            if name.endswith(extension):
+                print(f"moving file: {name}")
 
 # Use the watchdog library to detect changes in the file system and move files
 if __name__ == "__main__":
