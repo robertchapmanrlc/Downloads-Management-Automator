154546edc373321d560f70174b4b50089d76cbf2
Robert Chapman <chapmanrobert12@gmail.com>
2023-12-15 15:48:55 -0600

    Refactored the code to be more clean and deal with unsupported types

--- automation.py
+++ automation.py
@@ -41,6 +41,18 @@ def make_unique_name(name, dest):
 
     return name
 
+# This function determines the type of a file depending on the extension
+def file_type(extension):
+    if extension in images_extensions:
+        return "image"
+    elif extension in audio_extensions:
+        return "audio"
+    elif extension in videos_extensions:
+        return "video"
+    elif extension in misc_extensions:
+        return "misc"
+    return None
+
 # This function moves the file with the given name to the destination
 def move_file(name, file, dest):
     # If the file already exists, then rename it
@@ -58,38 +70,39 @@ class MoveHandler(FileSystemEventHandler):
     def on_modified(self, event):
         with scandir(source_folder) as files:
             for file in files:
-                self.handle_image_files(file, file.name)
-                self.handle_audio_files(file, file.name)
-                self.handle_video_files(file, file.name)
-                self.handle_misc_files(file, file.name)
+                # Call the appropriate function based on the extension
+                filename, extension = splitext(file.name)
+                match file_type(extension):
+                    case 'image':
+                        self.handle_image_files(file, filename)
+                    case 'audio':
+                        self.handle_audio_files(file, filename)
+                    case 'video':
+                        self.handle_video_files(file, filename)
+                    case 'misc':
+                        self.handle_misc_files(file, filename)
+                    case _:
+                        print(f"{extension} file type not supported for {filename}")
     
     # This method detects and move images files
     def handle_image_files(self, file, name):
-        for extension in images_extensions:
-            if name.endswith(extension):
-                move_file(name, file, images_destination_folder)
-                logging.info(f"Moved image file: {name}")
+        move_file(name, file, images_destination_folder)
+        logging.info(f"Moved image file: {name}")
 
     # This method detects and move audio files
     def handle_audio_files(self, file, name):
-        for extension in audio_extensions:
-            if name.endswith(extension):
-                move_file(name, file, audio_destination_folder)
-                logging.info(f"Moved audio file: {name}")
+        move_file(name, file, audio_destination_folder)
+        logging.info(f"Moved audio file: {name}")
 
     # This method detects and move video files
     def handle_video_files(self, file, name):
-        for extension in videos_extensions:
-            if name.endswith(extension):
-                move_file(name, file, video_destination_folder)
-                logging.info(f"Moved video file: {name}")
+        move_file(name, file, video_destination_folder)
+        logging.info(f"Moved video file: {name}")
 
     # This method detects and move various miscellaneous files
     def handle_misc_files(self, file, name):
-        for extension in misc_extensions:
-            if name.endswith(extension):
-                move_file(name, file, misc_destination_folder)
-                logging.info(f"Moved file: {name}")
+        move_file(name, file, misc_destination_folder)
+        logging.info(f"Moved file: {name}")
 
 # Use the watchdog library to detect changes in the file system and move files
 if __name__ == "__main__":
